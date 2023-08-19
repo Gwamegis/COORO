ce0e166722402acb891904a20e97adb687099de8
Kyubo Shim <didlaak6000@naver.com>
2023-08-19 09:58:00 +0900

    inital commit

---
+++ Selkhy/SelkhyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelkhyApp.swift
+//  Selkhy
+//
+//  Created by Kyubo Shim on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct SelkhyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
